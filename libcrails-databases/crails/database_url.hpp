2a8a4c9f9e0b59267c115b75c653e3dfc1a083bc
plaristote <michael@unetresgrossebite.com>
2024-02-08 17:21:56 +0100

    * DatabaseUrl uses string_view instead of string * DatabaseUrl can now be used to both parse and generate database urls

--- libcrails-databases/crails/database_url.hpp
+++ libcrails-databases/crails/database_url.hpp
@@ -3,77 +3,59 @@
 
 # include <boost/lexical_cast.hpp>
 # include <crails/logger.hpp>
+# include <string_view>
 
 namespace Crails
 {
   struct DatabaseUrl
   {
+    DatabaseUrl() {}
     DatabaseUrl(const char* str)
     {
       if (str)
         initialize(str);
     }
 
-    void initialize(const std::string& url)
-    {
-      if (url.size() > 0)
-      {
-        try
-        {
-          type     = substr(url, get_protocol_range(url));
-          hostname = substr(url, get_hostname_range(url));
-          username = substr(url, get_username_range(url));
-          password = substr(url, get_password_range(url));
-          port     = boost::lexical_cast<unsigned short>(substr(url, get_port_range(url)));
-          database_name = substr(url, get_database_name_range(url));
-        }
-        catch (std::exception& e)
-        {
-          logger << "Failed to read database url string " << url << Logger::endl;
-          throw e;
-        }
-      }
-    }
+    void initialize(const std::string_view url);
+    std::string to_string() const;
+    operator std::string() const { return to_string(); }
 
-    std::string    type, hostname, username, password, database_name;
-    unsigned int   port;
+    std::string  type, hostname, username, password, database_name;
+    unsigned int port;
 
   private:
-    std::string substr(const std::string& url, std::pair<int,int> range)
-    {
-      return url.substr(range.first, range.second - range.first);
-    }
+    std::string_view substr(const std::string_view url, std::pair<int,int> range);
 
-    std::pair<int, int> get_protocol_range(const std::string& url)
+    std::pair<int, int> get_protocol_range(const std::string_view url)
     {
       return { 0, url.find("://") };
     }
 
-    std::pair<int, int> get_username_range(const std::string& url)
+    std::pair<int, int> get_username_range(const std::string_view url)
     {
       int start_name = get_protocol_range(url).second + 3;
       return { start_name, url.find(":", start_name) };
     }
 
-    std::pair<int, int> get_password_range(const std::string& url)
+    std::pair<int, int> get_password_range(const std::string_view url)
     {
       int start_password = get_username_range(url).second + 1;
       return { start_password, url.find("@", start_password) };
     }
 
-    std::pair<int, int> get_hostname_range(const std::string& url)
+    std::pair<int, int> get_hostname_range(const std::string_view url)
     {
       int start_hostname = get_password_range(url).second + 1;
       return { start_hostname, url.find(":", start_hostname) };
     }
 
-    std::pair<int, int> get_port_range(const std::string& url)
+    std::pair<int, int> get_port_range(const std::string_view url)
     {
       int start_port = get_hostname_range(url).second + 1;
       return { start_port, url.find("/", start_port) };
     }
 
-    std::pair<int, int> get_database_name_range(const std::string& url)
+    std::pair<int, int> get_database_name_range(const std::string_view url)
     {
       int start_database_name = get_port_range(url).second + 1;
       return { start_database_name, url.length() };
